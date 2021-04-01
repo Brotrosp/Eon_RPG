e25f7056c43139c01c47724d9ac4fa57b6acdd7e
Brotrosp <81241705+Brotrosp@users.noreply.github.com>
2021-04-01 10:44:20 +0200

    01/04 options

---
+++ Eon_RPG/Assets/Scripts/Spell/Fireball.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+
+    public GameObject FireballText;
+
+    public void Start()
+    {
+        FireballText.SetActive(false);
+    }
+
+    public void OnMouseOver()
+    {
+        FireballText.SetActive(true);
+    }
+
+    public void OnMouseExit()
+    {
+        FireballText.SetActive(false);
+    }
+}
