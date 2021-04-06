5e447c7514d6839a79406438b2d449793afc79f0
0xmrt <vignoli@fitstic-edu.com>
2021-04-06 11:45:31 +0100

    Updated last commit

--- Eon_RPG/Assets/Scripts/RTSPlayerController.cs
+++ Eon_RPG/Assets/Scripts/RTSPlayerController.cs
@@ -26,24 +26,12 @@ public class RTSPlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-#if (UNITY_ANDROID || UNITY_IOS || UNITY_WP8 || UNITY_WP8_1) && !UNITY_EDITOR
-            //Handle mobile touch input
-            for (var i = 0; i < Input.touchCount; ++i)
-            {
-                Touch touch = Input.GetTouch(i);
 
-                if (touch.phase == TouchPhase.Began)
-                {
-                    MoveToTarget(touch.position);
-                }
-            }
-#else
         //Handle mouse input
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             MoveToTarget(Input.mousePosition);
         }
-#endif
 
         //Camera follow
         playerCamera.transform.position = Vector3.Lerp(playerCamera.transform.position, transform.position + cameraOffset, Time.deltaTime * 7.4f);
