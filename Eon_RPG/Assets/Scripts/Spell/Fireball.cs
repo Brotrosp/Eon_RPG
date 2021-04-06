a550b4849eedf41e267b9139f81deabf3d719493
Amonsage <fabio.marani98@gmail.com>
2021-04-06 12:00:42 +0200

    script abilità

--- Eon_RPG/Assets/Scripts/Spell/Fireball.cs
+++ Eon_RPG/Assets/Scripts/Spell/Fireball.cs
@@ -1,15 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Fireball : MonoBehaviour
 {
 
     public GameObject FireballText;
-
+    
     public void Start()
     {
         FireballText.SetActive(false);
+        
     }
 
     public void OnMouseOver()
@@ -21,4 +23,6 @@ public class Fireball : MonoBehaviour
     {
         FireballText.SetActive(false);
     }
+
+    
 }
