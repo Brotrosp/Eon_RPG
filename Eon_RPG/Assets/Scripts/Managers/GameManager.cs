dfa17bf608c8f3613a6be55efe945c6a4e42c274
Brotrosp <81241705+Brotrosp@users.noreply.github.com>
2021-04-28 14:00:45 +0200

    scene manager update

--- Eon_RPG/Assets/Scripts/Managers/GameManager.cs
+++ Eon_RPG/Assets/Scripts/Managers/GameManager.cs
@@ -1,75 +1,18 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField]
-
-    private int energy = 0;
-    private int healt = 0;
-
-    public enum GameState { EASY_BOSS }
-
-    public delegate void OnStateChangeHandler();
-
-    protected GameManager() { }
-    private static GameManager instance = null;
-    public event OnStateChangeHandler OnStateChange;
-    public GameState gameState { get; private set; }
-
-    public static GameManager Instance
-    {
-        get
-        {
-            if(GameManager.instance == null)
-            {
-                DontDestroyOnLoad(GameManager.instance);
-                GameManager.instance = new GameManager();
-            }
-            return GameManager.instance;
-        }
-    }
-
-
-
-    //public AudioManager audioManager;
-    //public UIManager uiManager;
-
+    // Start is called before the first frame update
     void Start()
     {
-        if(PlayerPrefs.HasKey("healt"))
-        {
-            healt = PlayerPrefs.GetInt("healt");
-        }
-        //audioManager = GameObject.Find("Managers").GetComponent<AudioManager>();
-        //uiManager = GameObject.Find("Managers").GetComponent<UIManager>();
+        
     }
 
-    public void SetGameState(GameState state)
+    // Update is called once per frame
+    void Update()
     {
-        this.gameState = state;
-        OnStateChange();
-    }
-
-    public void WriteOnScreen(int healt)
-    {
-
-    }
-
-    public void OnApplicationQuit()
-    {
-        GameManager.instance = null;
-    }
-    public void YouWin()
-    {
-        Debug.Log("Winner!!!");
-        SwitchSceneScript.OpenScene("Start");
+        
     }
 }
-
-
-
-
