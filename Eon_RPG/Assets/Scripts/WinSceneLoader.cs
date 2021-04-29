08822b7a59456609c779619213f956c89eff8b3d
Amonsage <fabio.marani98@gmail.com>
2021-04-29 13:45:13 +0200

    Final

--- Eon_RPG/Assets/Scripts/WinSceneLoader.cs
+++ Eon_RPG/Assets/Scripts/WinSceneLoader.cs
@@ -1,26 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class MainSceneLoader : MonoBehaviour
+using UnityEngine.SceneManagement;
+public class WinSceneLoader : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
             SceneScript.YouWin();
+        }
+        else if (other.gameObject.tag == "NextScene")
+        {
+            SceneScript.Restart();
         }
     }
 }
